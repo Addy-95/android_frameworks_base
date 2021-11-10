Project-Xtended/frameworks_base

PowerManagerService: Send broadcast when going to sleep

core/java/com/android/internal/util/xtended/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.xtended.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "xtended.intent.action.GO_TO_SLEEP";
+}

Addy-95/android_frameworks_base

core/java/com/android/internal/util/cherish/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.cherish.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "cherish.intent.action.GO_TO_SLEEP";
+}
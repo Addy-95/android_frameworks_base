e71c267b57141ef070faad8566420e43afc2b258
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-11-08 16:04:02 +0200

    PowerManagerService: Send broadcast when going to sleep

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1951,10 +1951,18 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
     }
 
-    private void dreamDisplayGroup(int groupId, long eventTime, int uid) {
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.cherish.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
+    }
+
+    private void napInternal(long eventTime, int uid) {
         synchronized (mLock) {
             if (dreamDisplayGroupNoUpdateLocked(groupId, eventTime, uid)) {
                 updatePowerStateLocked();
